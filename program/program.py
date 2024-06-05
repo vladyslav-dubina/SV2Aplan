2798d51363900b93dd60dfbecec0a13b69c375af
vladyslav-dubina <vladyslav.dubina@litsoft.com.ua>
2024-06-05 20:31:04 +0300

    add check for non block elements in beh creaton

--- program/program.py
+++ program/program.py
@@ -163,7 +163,9 @@ class Program():
 
         behaviour += 'B = ({})'.format(self.module.getBehInitProtocols())
         behaviour += ',' + self.module.getStructuresInStrFormat()
-        behaviour += ',' + self.module.getNotBlockElementsInStrFormat()
+
+        if (self.module.isIncludeNonBlockElements()):
+            behaviour += ',' + self.module.getNotBlockElementsInStrFormat()
 
         self.writeToFile(self.path_to_result + 'project.behp', behaviour)
         printWithColor('.beh file created \n', Color.PURPLE)
