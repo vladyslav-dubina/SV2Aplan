658123efe140a3a7c964f9a955c4149c7c154db3
Vlad Dubina <82110791+vladyslav-dubina@users.noreply.github.com>
2024-06-11 14:41:29 +0300

    Fix bugs in translation (#12)
    * add deadlock to assert
    
    * Format files
    
    * Fix if bodies
    
    * Add brackets after tilda in conditions
    
    * format utils

--- listener/system_verilog_listener.py
+++ listener/system_verilog_listener.py
@@ -1,6 +1,10 @@
 from antlr4_verilog.systemverilog import SystemVerilogParserListener
 
-from translator.system_verilog_to_aplan import SV2aplan, extractVectorSize, vectorSize2Aplan
+from translator.system_verilog_to_aplan import (
+    SV2aplan,
+    extractVectorSize,
+    vectorSize2Aplan,
+)
 from structures.aplan import Declaration, DeclTypes, Module, CounterTypes, Protocol
 
 
@@ -9,72 +13,83 @@ class SVListener(SystemVerilogParserListener):
         self.module = None
 
     def enterModule_declaration(self, ctx):
-        if (ctx.module_ansi_header() is not None):
-            self.module = Module(
-                ctx.module_ansi_header().module_identifier().getText())
+        if ctx.module_ansi_header() is not None:
+            self.module = Module(ctx.module_ansi_header().module_identifier().getText())
 
     def exitData_declaration(self, ctx):
-        assign_name = ''
+        assign_name = ""
         if ctx.data_type_or_implicit():
             data_type = ctx.data_type_or_implicit().getText()
-            index = data_type.find('reg')
+            index = data_type.find("reg")
             if index != -1:
-                for elem in ctx.list_of_variable_decl_assignments().variable_decl_assignment():
+                for (
+                    elem
+                ) in ctx.list_of_variable_decl_assignments().variable_decl_assignment():
                     identifier = elem.variable_identifier().identifier().getText()
-                    if (elem.expression()):
+                    if elem.expression():
                         expression = elem.expression().getText()
-                        if (expression):
+                        if expression:
                             sv2aplan = SV2aplan(self.module)
                             assign_name = sv2aplan.declaration2Aplan(elem)
-                    if (not assign_name):
-                        assign_name = ''
-                    self.module.declarations.append(Declaration(
-                        DeclTypes.REG, identifier, assign_name, 0))
+                    if not assign_name:
+                        assign_name = ""
+                    self.module.declarations.append(
+                        Declaration(DeclTypes.REG, identifier, assign_name, 0)
+                    )
 
     def exitNet_declaration(self, ctx):
-        assign_name = ''
-        if (ctx.net_type().getText() == 'wire'):
+        assign_name = ""
+        if ctx.net_type().getText() == "wire":
             for elem in ctx.list_of_net_decl_assignments().net_decl_assignment():
                 identifier = elem.net_identifier().identifier().getText()
                 self.module.internal_signals.append(identifier)
-                if (elem.expression()):
+                if elem.expression():
                     expression = elem.expression().getText()
-                    if (expression):
+                    if expression:
                         sv2aplan = SV2aplan(self.module)
                         assign_name = sv2aplan.declaration2Aplan(elem)
-                    if (not assign_name):
-                        assign_name = ''
-                    self.module.declarations.append(Declaration(
-                        DeclTypes.WIRE, identifier, assign_name, 0))
+                    if not assign_name:
+                        assign_name = ""
+                    self.module.declarations.append(
+                        Declaration(DeclTypes.WIRE, identifier, assign_name, 0)
+                    )
 
     def exitAnsi_port_declaration(self, ctx):
         header = ctx.net_port_header().getText()
-        index = header.find('input')
+        index = header.find("input")
         if index != -1:
             vector_size = extractVectorSize(header)
-            if (vector_size is None):
+            if vector_size is None:
                 port = Declaration(
-                    DeclTypes.INPORT, ctx.port_identifier().getText(), '', 0)
+                    DeclTypes.INPORT, ctx.port_identifier().getText(), "", 0
+                )
                 self.module.declarations.append(port)
             else:
-                aplan_vector_size = vectorSize2Aplan(
-                    vector_size[0], vector_size[1])
-                port = Declaration(DeclTypes.INPORT, ctx.port_identifier().getText(), '',
-                                   aplan_vector_size[0])
+                aplan_vector_size = vectorSize2Aplan(vector_size[0], vector_size[1])
+                port = Declaration(
+                    DeclTypes.INPORT,
+                    ctx.port_identifier().getText(),
+                    "",
+                    aplan_vector_size[0],
+                )
                 self.module.declarations.append(port)
 
-        index = header.find('output')
+        index = header.find("output")
         if index != -1:
             vector_size = extractVectorSize(header)
-            if (vector_size is None):
+            if vector_size is None:
                 port = Declaration(
-                    DeclTypes.OUTPORT, ctx.port_identifier().getText(), '', 0)
+                    DeclTypes.OUTPORT, ctx.port_identifier().getText(), "", 0
+                )
                 self.module.declarations.append(port)
             else:
-                aplan_vector_size = vectorSize2Aplan(
-                    vector_size[0], vector_size[1])
-                port = Declaration(DeclTypes.OUTPORT, ctx.port_identifier().getText(), '',
-                                   aplan_vector_size[0])
+                aplan_vector_size = vectorSize2Aplan(vector_size[0], vector_size[1])
+                port = Declaration(
+                    DeclTypes.OUTPORT,
+                    ctx.port_identifier().getText(),
+                    "",
+                    aplan_vector_size[0],
+                )
                 self.module.declarations.append(port)
 
     def enterAlways_construct(self, ctx):
@@ -83,10 +98,10 @@ class SVListener(SystemVerilogParserListener):
 
     def exitAssert_property_statement(self, ctx):
         expression = ctx.property_spec()
-        if (expression is not None):
+        if expression is not None:
             sv2aplan = SV2aplan(self.module)
             assert_name = sv2aplan.assert2Aplan(expression.getText())
-            assert_b = 'assert_B_{}'.format(self.module.assert_counter)
+            assert_b = "assert_B_{}".format(self.module.assert_counter)
             struct_assert = Protocol(assert_b)
-            struct_assert.addBody('{0}.Delta + !{0}'.format(assert_name))
+            struct_assert.addBody("{0}.Delta + !{0}.0".format(assert_name))
             self.module.notBlockElements.append(struct_assert)
